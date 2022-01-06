cc87abdc005a2ab2f4e1ac9cd0a75886a62eac57
Álisson Inácio <69402669+Alissoninacio1@users.noreply.github.com>
2022-01-05 17:27:22 -0700

    Add files via upload

---
+++ teach-01/src/com/company/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+        hello.sayHello();
+    }
+
+    public void sayHello() {
+        System.out.println("Hello World!");
+    }
+}
